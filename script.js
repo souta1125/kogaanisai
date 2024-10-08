1bb02285d6feaaa1314c8dbd67ad8b22ecea2073
souta1125 <97822981+souta1125@users.noreply.github.com>
2024-10-08 12:33:13 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ $(window).load(function(){
   let w = $('.t-body').width();
   $('.t-body').attr('style', "--i:" + w + "px");
 })
+
+$(window).resize(function(){
+  let w = $('.t-body').width();
+  $('.t-body').attr('style', "--i:" + w + "px");
+})
