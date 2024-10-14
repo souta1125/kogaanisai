802d2fda3f2f2151e6a8e589afc6641a99a142b2
souta1125 <97822981+souta1125@users.noreply.github.com>
2024-10-15 08:57:51 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-$(window).load(function(){
-  let w = $('.t-body').width();
-  $('.t-body').attr('style', "--i:" + w + "px");
-})
+$('.list-day-item > button').click(function(){
+  let list = $('.list-day')
+  ,   body = $('.t-content-body')
+  ,   items = body.find('.t-item')
+  ,   field = $(this).attr('data-field');
 
-$(window).resize(function(){
-  let w = $('.t-body').width();
-  $('.t-body').attr('style', "--i:" + w + "px");
+  list.find('.list-day-item > button').attr('aria-disabled', true);
+  $(this).attr('aria-disabled', false);
+
+  items.attr('aria-disabled', true);
+  for (let i = 0; i < items.length; i++) {
+    let item = body.find('.t-item').eq(i)
+    ,   day = item.attr('data-field');
+
+    if(field === day){
+      item.attr('aria-disabled', false);
+    }
+  }
 })
